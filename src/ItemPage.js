ca940ab2b6291a9138b8cb7a13e9655fa879e582
Juan <iamjuandel@gmail.com>
2022-05-13 12:17:23 -0400

    update2

--- src/ItemPage.js
+++ src/ItemPage.js
@@ -1,20 +1,26 @@
 import React from 'react'; 
 import PropTypes from 'prop-types'; 
-import './ItemPage.css'; 
+import './ItemPage.css';
+import Item from './Item'; 
 
-function ItemPage({items}) { 
+function ItemPage({items, onAddToCart}) { 
     return ( 
         <ul className="ItemPage-items"> 
             {items.map(item => 
                 <li key={item.id} className="ItemPage-item"> 
-                    {item.name} 
+                    <Item item={item}>
+                        <button className="Item-addToCart" onClick={()=>onAddToCart(item)}>
+                        Add to Cart
+                        </button>
+                    </Item>
                 </li>)} 
         </ul> 
     ); 
 }
 
 ItemPage.propTypes = { 
-    items: PropTypes.array.isRequired
+    items: PropTypes.array.isRequired,
+    onAddToCart: PropTypes.func.isRequired 
 };
  
 export default ItemPage; 
